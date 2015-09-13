e1b55c36b52001f885150b54c7c6a00bec792a78
Chris Pope <cpope13@gmail.com>
2015-09-12 23:48:31 -0700

    Added rotating pictures on index.html

---
+++ static/full.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  
+  console.log('jquery is ready!');
+
+  var callTimer = setInterval(function(){rotatePics()}, 4500);
+
+  var theCount = 0;
+
+  function rotatePics() {
+    
+    console.log('the Count is:', theCount);
+    if (theCount == 0) {
+      $('.full').css({"background": "url('/static/engagement-near-water.JPG') no-repeat center center fixed", "-webkit-background-size" : "cover", "-moz-background-size": "cover", "background-size": "cover", "-o-background-size": "cover"});
+      theCount += 1;
+
+    } else {
+      $('.full').css({"background": "url('/static/kirin-chris-overlooking-vineyard.JPG') no-repeat center center fixed", "-webkit-background-size" : "cover", "-moz-background-size": "cover", "background-size": "cover", "-o-background-size": "cover"});
+      theCount -= 1;
+    }
+   
+  }
+
+  rotate();
+});
