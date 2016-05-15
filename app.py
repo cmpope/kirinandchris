73e36bf9d77b77a198a0cce70da7e800891e6497
Chris Pope <chris@cmpope.com>
2016-05-15 15:51:19 -0700

    updates to RSVP functionality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from forms import AddressForm
 import os
 import requests
+from fuzzywuzzy import process
 
 
 app = Flask(__name__)
@@ -20,23 +21,34 @@ def index():
 
 @app.route('/rsvp', methods=['GET', 'POST'])
 def rsvp():
+    choices = models.Party.query.all()
+    all_addresses = []
+    for x in choices:
+        all_addresses.append(x.address)
+    print all_addresses
     addressForm = AddressForm()
     address = None
     postal = None
+    check = None
     error = None
+    full_name = None
+    g = []
+    q = None
     if addressForm.validate_on_submit():
         address = addressForm.address.data
         #session['address'] = addressForm.address.data
-        q = models.Party.query.filter_by(address=address).first()
-        print q
-        addressForm.address.data = ''
-        if q:
-            postal = q.postal
-            print postal
+        x = process.extractOne(address, all_addresses)
+        print x
+        if x[1] == 100:
+            q = models.Party.query.filter_by(address=address).first()
+            g = models.Guests.query.filter_by(party_id=q.id).all()
+            addressForm.address.data = ''
+        elif 85 < x[1] < 100:
+            check = 'Did you mean this address:' + ' ' + x[0]
             #return redirect(url_for('rsvp'))
         else:
-            error = "Sorry, we did not invite that address"
-    return render_template('rsvp.html', addressForm=addressForm, addres=address, postal=postal, error=error)
+            error = "Sorry, we don't recognize that address, please enter your address as it appears on your invite"
+    return render_template('rsvp.html', addressForm=addressForm, addres=address, postal=postal, full_name=full_name, g=g, q=q, check=check, error=error)
 
 
 @app.route('/<name>')
