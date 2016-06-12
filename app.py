3d1a12821073600f98596e738db2bbf8e0f93247
Chris Pope <chris@cmpope.com>
2016-06-12 00:00:03 -0700

    mirror app.py with kirinandhchris.py

--- app.py
+++ app.py
@@ -13,10 +13,46 @@ db = SQLAlchemy(app)
 
 import models
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html')
+
+@app.route('/the-wedding-party')
+def the_wedding_party():
+    return render_template('the-wedding-party.html')
+
+@app.route('/the-big-day')
+def the_big_day():
+    return render_template('the-big-day.html')
+
+@app.route('/memories')
+def memories():
+    return render_template('memories.html')
+
+@app.route('/registry')
+def registry():
+    return render_template('registry.html')
+
+@app.route('/robots.txt')
+def robots():
+    return app.send_static_file('robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return app.send_static_file('sitemap.xml')
 
 
 @app.route('/rsvp', methods=['GET', 'POST'])
